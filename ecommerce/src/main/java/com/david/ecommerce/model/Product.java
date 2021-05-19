b15345047de5faff5b86788c4fd659f7b1e7615f
David Arruda <deh0_@hotmail.com>
2021-05-18 21:07:24 -0300

    criado relacionamento com a classe image

--- ecommerce/src/main/java/com/david/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/david/ecommerce/model/Product.java
@@ -1,48 +1,60 @@
 package com.david.ecommerce.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "products")
 public class Product {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private String id;
-	
-	@Column(length = 120)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 120, nullable = false)
 	private String name;
 	
+	@Column(columnDefinition = "text", unique = true)
+	private String ean;
+
 	@Column(columnDefinition = "text")
 	private String description;
-	
-	@Column(precision = 15, scale = 2)
+
+	@Column(precision = 15, scale = 2, nullable = false)
 	private BigDecimal price;
+
+	@OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
+	private List<ProductHasCategory> categories;
 	
+	@Column(columnDefinition = "text")
+	private List<String> images; 
+
 	public Product() {
-		// CONSTRUCTOR VAZIO 
+		// CONSTRUCTOR VAZIO
 	}
 
-	public Product(String id, String name, String description, BigDecimal price) {
-		super();
+	public Product(Long id, String name, String description, BigDecimal price, List<ProductHasCategory> categories) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
+		this.categories = categories;
 	}
 
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -69,5 +81,46 @@ public class Product {
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
-	
+
+	public List<ProductHasCategory> getCategories() {
+		return categories;
+	}
+
+	public ProductHasCategory addCategori(ProductHasCategory categorie) {
+		getCategories().add(categorie);
+		categorie.setProduct(this);
+		return categorie;
+	}
+
+	public ProductHasCategory removeApontamento(ProductHasCategory categorie) {
+		getCategories().remove(categorie);
+		categorie.setProduct(null);
+		return categorie;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
