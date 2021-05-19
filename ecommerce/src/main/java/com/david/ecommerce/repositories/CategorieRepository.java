e77b3b8acfa5f40331741be843b5a204990ca0df
David Arruda <deh0_@hotmail.com>
2021-05-18 21:06:01 -0300

    Criação repository de Category

---
+++ ecommerce/src/main/java/com/david/ecommerce/repositories/CategorieRepository.java
@@ -0,0 +1,7 @@
+package com.david.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategorieRepository extends JpaRepository<CategorieRepository, Long>{
+
+}
