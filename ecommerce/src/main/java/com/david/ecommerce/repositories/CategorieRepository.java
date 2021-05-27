8f734adc83a000f549ee73e947d7722d2e278b60
David Arruda <deh0_@hotmail.com>
2021-05-27 00:04:52 -0300

    Repositorio de Categoria

--- ecommerce/src/main/java/com/david/ecommerce/repositories/CategorieRepository.java
+++ ecommerce/src/main/java/com/david/ecommerce/repositories/CategorieRepository.java
@@ -1,7 +1,13 @@
 package com.david.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface CategorieRepository extends JpaRepository<CategorieRepository, Long>{
+import com.david.ecommerce.model.Category;
+
+@Repository
+@Transactional
+public interface CategorieRepository extends JpaRepository<Category, Long>{
 
 }
