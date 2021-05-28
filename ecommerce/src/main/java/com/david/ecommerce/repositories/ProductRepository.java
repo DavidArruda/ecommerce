29f6fe7b0a6dba0c99d1d9a9a76feae6f3bee09a
David Arruda <deh0_@hotmail.com>
2021-05-28 20:31:58 -0300

    Adicionado anotação @Transactiol, e criado método findByEan

--- ecommerce/src/main/java/com/david/ecommerce/repositories/ProductRepository.java
+++ ecommerce/src/main/java/com/david/ecommerce/repositories/ProductRepository.java
@@ -2,10 +2,13 @@ package com.david.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.david.ecommerce.model.Product;
 
 @Repository
+@Transactional
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	Product findByEan(String ean);
 }
