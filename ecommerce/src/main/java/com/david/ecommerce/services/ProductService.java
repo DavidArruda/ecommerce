c03427ef96a6ed0372c9b3ff40e0b9ceec2d3885
David Arruda <deh0_@hotmail.com>
2021-05-28 20:30:37 -0300

    Criação do service de produts

---
+++ ecommerce/src/main/java/com/david/ecommerce/services/ProductService.java
@@ -0,0 +1,69 @@
+package com.david.ecommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.david.ecommerce.exception.EntityNotFoundException;
+import com.david.ecommerce.exception.ProblemType;
+import com.david.ecommerce.model.Image;
+import com.david.ecommerce.model.Product;
+import com.david.ecommerce.repositories.CategorieRepository;
+import com.david.ecommerce.repositories.ImageRepository;
+import com.david.ecommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	private final ProductRepository repository;
+	private final CategorieRepository repositoryCateg;
+	private final ImageRepository repositoryImg;
+
+	public ProductService(@Autowired ProductRepository repository, @Autowired CategorieRepository repositoryCateg, ImageRepository repositoryImg) {
+		this.repository = repository;
+		this.repositoryCateg = repositoryCateg;
+		this.repositoryImg = repositoryImg;
+	}
+
+	public Product save(Product product) {
+		if (!repositoryCateg.existsById(product.getCategory().getId())) {
+			throw new EntityNotFoundException(ProblemType.CATEGORY_NOT_FOUND.getDetail());
+		}
+		
+		repository.save(product);
+		
+		if (product.getImages() != null && !product.getImages().isEmpty()) {
+			for (Image img : product.getImages()) {
+				img.setProduct(product);
+				repositoryImg.save(img);
+			}	
+		}
+		
+		return product;
+	}
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(long id) {
+		return repository.findById(id).orElseThrow(() -> new EntityNotFoundException(ProblemType.PRODUCT_NOT_FOUND.getDetail()));
+	}
+
+	public Product update(Product product) {
+		if (!repository.existsById(product.getId())) {
+			throw new EntityNotFoundException(ProblemType.PRODUCT_NOT_FOUND.getDetail());
+		}
+
+		return repository.save(product);
+	}
+
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new EntityNotFoundException(ProblemType.PRODUCT_NOT_FOUND.getDetail());
+		}
+		repository.deleteById(id);
+	}
+
+}
