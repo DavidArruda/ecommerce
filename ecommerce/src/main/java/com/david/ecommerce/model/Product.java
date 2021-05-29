a062e4986d627451c323ef5256c5c8e56aa17e84
David Arruda <deh0_@hotmail.com>
2021-05-28 23:52:15 -0300

    Adicionado validações em alguns atributos

--- ecommerce/src/main/java/com/david/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/david/ecommerce/model/Product.java
@@ -18,6 +18,11 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "products")
@@ -27,28 +32,38 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank(message = "Campo name não pode ser nulo ou vazio")
+	@Size(min = 3, max = 120, message = "Campo nome deve conter entre 3 e 120 caracteres")
 	@Column(length = 120, nullable = false)
 	private String name;
 
+	@NotBlank(message = "Campo sku não pode ser nulo ou vazio")
+	@Size(min = 3, max = 120, message = "Campo sku de conter entre 3 e 20 caracteres")
 	@Column(length = 20, nullable = false, unique = true)
 	private String sku;
 
-	private Boolean status;
+	private Boolean status = false;
 
 	@Column(columnDefinition = "text", unique = true, nullable = false)
 	private String ean;
 
+	@Size(min = 1, max = 90, message = "Campo brand deve conter entre 1 e 90 caracteres")
 	@Column(length = 90)
 	private String brand;
 
 	@Column(columnDefinition = "text", nullable = false)
 	private String description;
 
+	@Min(value = 0, message = "Campo qty deve ser >= 0")
 	private Integer qty;
 
+	@DecimalMin(value = "0.0", message = "Campo costPrice deve ser maior >= 0") 
+	@Digits(integer = 15, fraction = 4, message = " Campo costPrice fora do limite. Esperado: numeric 15,4")
 	@Column(precision = 15, scale = 4, nullable = false)
 	private BigDecimal costPrice;
 
+	@DecimalMin(value = "0.0", message = "Campo price deve ser maior >= 0")
+	@Digits(integer = 15, fraction = 4, message = " Campo price fora do limite. Esperado: numeric 15,4")
 	@Column(precision = 15, scale = 4, nullable = false)
 	private BigDecimal price;
 
